1cc436843480edf0d3f9ba78487829388468c917
Usuario <Usuario@hp>
2016-09-11 20:10:47 -0400

    actulizado y funcionando

--- src/java/controlador/PersonaControlador.java
+++ src/java/controlador/PersonaControlador.java
@@ -15,33 +15,22 @@ import modelo.Persona;
  *
  * @author Usuario
  */
-public class PersonaControlador {
-    
-    private Statement st;
+public class PersonaControlador extends Conexion{
+   
     private ResultSet rs;
-    private Connection cnn;
-    boolean ok;
 
-    Conexion conexion = new Conexion();
-            
     public PersonaControlador() {
-         try{
-            conexion.getConexion(); // se intenta conectar a la base de datos
-         }catch(Exception e){
-          System.out.println("Error al conectar "+e.getMessage());
-         }
+         this.getConexion();
     }
     
     public boolean insertar(Persona per){
      
         try{
-            System.out.println("NOMBRE "+per.getNombre());
-            System.out.println("EDAD "+per.getEdad());
-            String query = "insert into persona values ('2', '"+per.getNombre()+"', '"+per.getEdad()+"')";
+            //getStatement();
+            String query = "insert into persona values ('"+per.getId()+"','"+per.getNombre()+"', '"+per.getEdad()+"')";
             System.out.println("QUERY "+query);
-            st = cnn.createStatement();
             st.executeUpdate(query); //como es un insert se utiliza executeUpdate, si era un select executeQuery
-            conexion.cerrarSesion();
+            //this.cerrarSesion();
             return true;
         }catch(SQLException ex){
             System.out.println("Error al insertar "+ex.getMessage());
@@ -56,11 +45,11 @@ public class PersonaControlador {
         
        try{
             String query = "Select * from persona";
-            st = cnn.createStatement();
             rs = st.executeQuery(query);
             while(rs.next()){
-                persona.add(new Persona(rs.getInt(1), rs.getString(2), rs.getInt(3)));
+               persona.add(new Persona(rs.getInt(1), rs.getString(2), rs.getInt(3)));
             }
+             return persona;
         }catch(SQLException ex){
             System.out.println("Error al consultar "+ex.getMessage());
             
